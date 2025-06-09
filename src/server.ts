56eef673521239996f5eb0ce7130555a7efa7bf6
Carlos Macias <lmaciasgonzalez@gmail.com>
2025-06-09 10:46:48 -0600

    feat(routes): add rest api routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve("pages/index.html"));
-});
+app.use("/api", router);
 
 export default app;

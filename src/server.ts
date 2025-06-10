c02339bf2e811838252a5461cfd0499ac5f26d89
Carlos Macias <lmaciasgonzalez@gmail.com>
2025-06-10 06:36:24 -0600

    feat(middleware): add cors on api

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import router from "./router";
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/api", router);
 
